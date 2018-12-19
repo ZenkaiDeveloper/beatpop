b2476be7d81edcede956983bcdc954ba49dd5dc6
Andy <wang.andy32@gmail.com>
2018-12-18 21:56:21 -0500

    added instructions

--- javascript/IntroPage.js
+++ javascript/IntroPage.js
@@ -64,6 +64,15 @@ class IntroPage extends Phaser.Scene {
 
     this.spaceJam = this.add.text(width/3,(height/2)-(80) , "Are you ready for this - Space Jam ", { fontSize: '42px', fill: '#eee', backgroundColor: "#000" });
 
+
+    this.instructions = this.add.text((width/8)-(150),height/2 , "Instructions", { fontSize: '23px', fill: '#50f70e', backgroundColor:'#000' });
+    this.instructions = this.add.text((width/8)-(150),height/2 , "Two conditions must be met to score", { fontSize: '30px', fill: '#50f70e', backgroundColor:'#000' });
+    this.instructionsOne = this.add.text((width/8)-(150), height/1.7 , "One: In order to score the paddle and a particle must collide", { fontSize: '23px', fill: '#50f70e', backgroundColor:'#000' });
+    this.instructionsOne = this.add.text((width/8)-(150), height/1.6 , "Two: You must press the corresponding letter indicated at the bottom of the screen on collision.", { fontSize: '23px', fill: '#50f70e', backgroundColor:'#000' });
+    this.instructionsOne = this.add.text((width/8)-(150), height/1.5 , "If you score the paddle grows in size, otherwise the particle falls through the screen and the paddle shrinks", { fontSize: '23px', fill: '#50f70e', backgroundColor:'#000' });
+    this.instructionsOne = this.add.text((width/8)-(150), height/1.4 , "If the paddle gets too small and dissappears, the game is over.", { fontSize: '23px', fill: '#50f70e', backgroundColor:'#000' });
+    this.instructionsOne = this.add.text((width/8)-(150), height/1.4 , "Good Luck and feel free to clone down to add your own songs :) ", { fontSize: '23px', fill: '#50f70e', backgroundColor:'#000' });
+
     this.usoSong.setInteractive();
     this.burnSong.setInteractive();
     this.spaceJam.setInteractive();
